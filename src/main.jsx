ece89321370cb308eb0073d7cd5ae6e733cb3a7f
Gonzalo Ibarrola <gonzaloibarrola95@gmail.com>
2026-01-28 18:12:34 -0300

    gh

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+const basename = process.env.PUBLIC_URL;
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
